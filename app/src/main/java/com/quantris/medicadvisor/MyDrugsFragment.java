6c270e5296c0965abfa6ee8afc3be7f52e3b717f
sebarena <sebarena@gmail.com>
2018-06-07 09:43:29 +0200

    Initial files published

---
+++ app/src/main/java/com/quantris/medicadvisor/MyDrugsFragment.java
@@ -0,0 +1,18 @@
+package com.quantris.medicadvisor;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyDrugsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mydrugs, null);
+        //return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
